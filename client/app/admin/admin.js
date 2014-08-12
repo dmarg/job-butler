4d6d58f5b5bfae882e6ba4af4b8acc0bb90d68f7
Daniel Margol <daniel.margol@gmail.com>
2014-08-12 13:03:33 -0400

    First Commit: Initialized Project with Yo Angular Fullstack Generator and set up Google OAuth for logging in.

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jobButlerApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
