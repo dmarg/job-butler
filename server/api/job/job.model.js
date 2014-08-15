371be814c0206d319ce6655c53871398b759ab81
Daniel Margol <daniel.margol@gmail.com>
2014-08-15 11:37:17 -0400

    Created a child schema for Stage in the JobSchema Model.

--- server/api/job/job.model.js
+++ server/api/job/job.model.js
@@ -3,13 +3,20 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var StageSchema = new Schema({
+  stageName: String,
+  date: Date,
+  unixTC: String,
+  notes: Array
+});
+
 var JobSchema = new Schema({
   _userId: {type: Schema.Types.ObjectId, ref: 'User' , index: true},
   companyName: String,
   linkToJobPost: String,
   positionTitle: String,
   description: String,
-  stage: Array,
+  stage: [StageSchema],
   active: Boolean
 });
 
